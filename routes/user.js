23d946f9534d53897c96f981fbcf96535231b956
eugenewo <j2006il@walla.co.il>
2021-10-25 18:33:37 +0300

    ..

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,8 @@
  const express = require("express");
-
-
  const bcrypt = require("bcrypt");
  const jwt = require("jsonwebtoken");
  const User = require("../models/user");
  const router = express.Router();
-
-
-
-
-
-
-
 
 
  router.post("/signup", (req, res, next) => {
